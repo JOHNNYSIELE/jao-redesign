ed76e60ceada60956760baec7e6d33f13d436b60
mistabenjo <benakehurst@hotmail.com>
2017-05-21 23:13:13 +0300

    about page

--- app/aboutModule.js
+++ app/aboutModule.js
@@ -1,10 +1,22 @@
-(function(){
+(function() {
 
     "use strict";
 
     var aboutModule = angular.module("aboutModule", []);
 
-    aboutModule.controller("AboutController", function($scope, $http){
+    aboutModule.controller("AboutController", function($scope, $http) {
+
+        $http
+            .get("assets/json/site.json")
+            .then(function(response) {
+
+                $scope.aboutTitle = response.data.about.aboutTitle;
+                $scope.aboutImage = response.data.about.aboutImage;
+                var aboutText = response.data.about.aboutText[0];
+                $scope.aboutText = aboutText;
+
+            });
+
 
     });
 
