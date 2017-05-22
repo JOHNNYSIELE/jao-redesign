e10bc359860cadd65599fcf85d3fb7f1ff22db1c
benAkehurst <benakehurst@hotmail.com>
2017-05-22 14:44:08 +0300

    single images
    Made single images apper with shadow with ngRepeat

--- app/natgModule.js
+++ app/natgModule.js
@@ -1,11 +1,20 @@
-(function(){
+(function () {
 
     "use strict";
 
     var natgModule = angular.module("natgModule", []);
 
-    natgModule.controller("NatGController", function($scope, $http){
+    natgModule.controller("NatGController", function ($scope, $http) {
+        $http
+            .get("assets/json/site.json")
+            .then(function (response) {
 
+                console.log(response.data.collage.nationalGeographic);
+                $scope.natGeoTitle = response.data.collage.nationalGeographic.nationalGeographicTitle;
+                $scope.natGeoText = response.data.collage.nationalGeographic.nationalGeographicText[0];
+                $scope.natGeoImages = response.data.collage.nationalGeographic.images;
+
+            });
     });
 
 })();
