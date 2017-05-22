4bf9a2fcfb8daa5ac833748d9e03aca17600dde7
benAkehurst <benakehurst@hotmail.com>
2017-05-22 11:03:00 +0300

    updated json
    updated json
    built basic page functionality
    added shadow to menu bar

--- app/btsModule.js
+++ app/btsModule.js
@@ -6,6 +6,17 @@
 
     btsModule.controller("BTSController", function($scope, $http){
 
+        $http
+            .get("assets/json/site.json")
+            .then(function(response) {
+
+               // console.log(response.data.collage.besideTheSea.images);
+               $scope.btsTitle = response.data.collage.besideTheSea.besideTheSeaTitle;
+               $scope.btsText = response.data.collage.besideTheSea.besideTheSeaText[0];
+               $scope.btsImages = response.data.collage.besideTheSea.images;
+
+            });
+
     });
 
 })();
