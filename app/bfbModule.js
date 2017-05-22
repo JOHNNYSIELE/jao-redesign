3e1ea1bbbc8d87673dda4ba12ee016905bd3526c
benAkehurst <benakehurst@hotmail.com>
2017-05-22 16:31:07 +0300

    bfb
    added bfb

--- app/bfbModule.js
+++ app/bfbModule.js
@@ -5,7 +5,16 @@
     var bfbModule = angular.module("bfbModule", []);
 
     bfbModule.controller("BFBController", function($scope, $http){
+        $http
+            .get("assets/json/site.json")
+            .then(function (response) {
 
+                console.log(response.data.collage.beautifulForBedtime);
+                $scope.beautifulForBedtimeTitle = response.data.collage.beautifulForBedtime.beautifulForBedtimeTitle;
+                $scope.beautifulForBedtimeText = response.data.collage.beautifulForBedtime.beautifulForBedtimeText[0];
+                $scope.beautifulForBedtimeImages = response.data.collage.beautifulForBedtime.images;
+
+            });
     });
 
 })();
