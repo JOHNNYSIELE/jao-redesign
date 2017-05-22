34b2340c18513e6ffa66cecea7b73b4c95c8be71
benAkehurst <benakehurst@hotmail.com>
2017-05-22 17:43:45 +0300

    bh
    bh

--- app/bhModule.js
+++ app/bhModule.js
@@ -5,7 +5,14 @@
     var bhModule = angular.module("bhModule", []);
 
     bhModule.controller("BHController", function($scope, $http){
+        $http
+            .get("assets/json/site.json")
+            .then(function (response) {
 
+                // console.log(response.data.photography.antigua);
+                $scope.beachHutsImages = response.data.photography.beachHuts.images;
+
+            });
     });
 
 })();
