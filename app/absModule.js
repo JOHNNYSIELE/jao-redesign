824480110bcae78e60f55ded38eed30ae4e61998
benAkehurst <benakehurst@hotmail.com>
2017-05-22 17:39:45 +0300

    abs
    abs

--- app/absModule.js
+++ app/absModule.js
@@ -5,7 +5,14 @@
     var absModule = angular.module("absModule", []);
 
     absModule.controller("ABSController", function($scope, $http){
+        $http
+            .get("assets/json/site.json")
+            .then(function (response) {
 
+                // console.log(response.data.photography.antigua);
+                $scope.abstractsImages = response.data.photography.abstracts.images;
+
+            });
     });
 
 })();
