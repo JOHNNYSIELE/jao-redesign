1dcb55be6ae8d3eb5e12d89c0ad2f58823a3262e
benAkehurst <benakehurst@hotmail.com>
2017-05-22 17:32:15 +0300

    aus
    aus

--- app/ausModule.js
+++ app/ausModule.js
@@ -5,7 +5,14 @@
     var ausModule = angular.module("ausModule", []);
 
     ausModule.controller("AUSController", function($scope, $http){
+        $http
+            .get("assets/json/site.json")
+            .then(function (response) {
 
+                // console.log(response.data.photography.australia);
+                $scope.australiaImages = response.data.photography.australia.images;
+
+            });
     });
 
 })();
