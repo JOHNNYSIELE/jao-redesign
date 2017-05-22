25027d32d9b3ef62fde40ec662a54bbe647fa2a1
benAkehurst <benakehurst@hotmail.com>
2017-05-22 17:23:27 +0300

    icw
    icw

--- app/icwModule.js
+++ app/icwModule.js
@@ -5,7 +5,16 @@
     var icwModule = angular.module("icwModule", []);
 
     icwModule.controller("ICWController", function($scope, $http){
+        $http
+            .get("assets/json/site.json")
+            .then(function (response) {
 
+                console.log(response.data.collage.iCanWrite);
+                $scope.iCanWriteTitle = response.data.collage.iCanWrite.iCanWriteTitle;
+                $scope.iCanWriteText = response.data.collage.iCanWrite.iCanWriteText[0];
+                $scope.iCanWriteImages = response.data.collage.iCanWrite.images;
+
+            });
     });
 
 })();
