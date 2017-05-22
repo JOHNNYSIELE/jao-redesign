f382708b827314786a6fa7475ab2bd6d61672676
benAkehurst <benakehurst@hotmail.com>
2017-05-22 17:36:58 +0300

    ant
    ant

--- app/antModule.js
+++ app/antModule.js
@@ -5,7 +5,14 @@
     var antModule = angular.module("antModule", []);
 
     antModule.controller("ANTController", function($scope, $http){
+        $http
+            .get("assets/json/site.json")
+            .then(function (response) {
 
+                // console.log(response.data.photography.antigua);
+                $scope.antiguaImages = response.data.photography.antigua.images;
+
+            });
     });
 
 })();
