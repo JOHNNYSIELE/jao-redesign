e7fc4429421534e106b0f4c29420a8162790ad00
benAkehurst <benakehurst@hotmail.com>
2017-05-22 16:04:57 +0300

    robots
    added robots page

--- app/robotsModule.js
+++ app/robotsModule.js
@@ -6,6 +6,16 @@
 
     robotsModule.controller("RobotsController", function($scope, $http){
 
+        $http
+            .get("assets/json/site.json")
+            .then(function (response) {
+
+                console.log(response.data.collage.robots);
+                $scope.robotsTitle = response.data.collage.robots.robotsTitle;
+                $scope.robotsText = response.data.collage.robots.robotsText[0];
+                $scope.robotsImages = response.data.collage.robots.images;
+
+            });
     });
 
 })();
